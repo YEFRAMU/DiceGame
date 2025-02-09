8bb5ab71009b83d72b7bbbdfd9e2376615af0906
YEFRAMU <vinceumali81@gmail.com>
2025-02-10 04:37:23 +0800

    improve queryselector format for img

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 const randomNumber1 = Math.floor(Math.random() * 6) + 1;
 console.log(randomNumber1);
-document.querySelector(".img1").setAttribute("src", "./images/dice" + randomNumber1 + ".png")
 
 const randomNumber2 = Math.floor(Math.random() * 6) + 1;
 console.log(randomNumber2);
+
+document.querySelector(".img1").setAttribute("src", "./images/dice" + randomNumber1 + ".png")
 document.querySelector(".img2").setAttribute("src", "./images/dice" + randomNumber2 + ".png")
 
 if (randomNumber1 > randomNumber2) {
